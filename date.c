113bbcc99aa9db4ef239ba1059b33223cab0d361
Roderik Ploszek <roderik.ploszek@gmail.com>
2019-04-01 08:53:24 +0200

    Fix date conversion

--- date.c
+++ date.c
@@ -6,5 +6,6 @@ int read_date(char* buffer, struct tm* tm)
   int ret = sscanf(buffer,
 		   "%d-%d-%d %*s",
 		   &tm->tm_year, &tm->tm_mon, &tm->tm_mday);
+  tm->tm_year -= 1900;
   return ret == 3;
 }
